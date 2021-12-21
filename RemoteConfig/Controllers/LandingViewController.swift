892e203ff8d607ecbd4c6de59c8855434493ac2b
Ali Dinc <mrdinc@outlook.com>
2021-12-21 14:00:00 +0000

    Landing View Controller added & required UI elements placed

--- RemoteConfig/Controllers/LandingViewController.swift
+++ RemoteConfig/Controllers/LandingViewController.swift
@@ -9,11 +9,79 @@ import Foundation
 import UIKit
 
 class LandingViewController: UIViewController {
-    
+
+    // MARK: - Properties
     private let imageView = RCImageView(frame: .zero)
-    
-    
-    
-    
-    
+    private lazy var captureButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(configureImagePicker(for:)), for: .touchUpInside)
+        button.setTitle("Capture Photo", for: .normal)
+        button.tag = 0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private lazy var pickFromLibraryButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(configureImagePicker(for:)), for: .touchUpInside)
+        button.setTitle("Pick From Library", for: .normal)
+        button.tag = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    // MARK: - Initialize
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+    // MARK: - Methods
+    private func configureView() {
+        view.backgroundColor = .lightGray
+        view.addSubview(imageView)
+        view.addSubview(captureButton)
+        view.addSubview(pickFromLibraryButton)
+        let padding: CGFloat = 20
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
+            captureButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: padding),
+            captureButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            pickFromLibraryButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: padding),
+            pickFromLibraryButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+        ])
+    }
+}
+
+// MARK: - UIImagePickerController
+extension LandingViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @objc private func configureImagePicker(for sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        setImagePickerSource(imagePicker, for: sender)
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    private func setImagePickerSource(_ imagePicker: UIImagePickerController, for sender: UIButton) {
+        if sender.tag == 0 && UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType          = .camera
+            imagePicker.showsCameraControls = true
+        } else if sender.tag == 1 && UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePicker.sourceType          = .photoLibrary
+        } else {
+            self.presentAlert(title: "Not available", message: "Please check your camera or photo library settings.", buttonTitle: "OK")
+        }
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            imageView.image = editedImage
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            imageView.image = originalImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- RemoteConfig/View/RCImageView.swift
+++ RemoteConfig/View/RCImageView.swift
@@ -23,7 +23,8 @@ class RCImageView: UIImageView {
     // MARK: - Methods
     private func configure() {
         layer.cornerRadius                        = 22
-        contentMode                               = .scaleAspectFill
+        self.frame.size                           = CGSize(width: 100, height: 100)
+        contentMode                               = .scaleAspectFit
         clipsToBounds                             = true
         image                                     = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
